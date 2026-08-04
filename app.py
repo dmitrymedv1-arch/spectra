8dbc58df3aaf59d014aa75af9eca07f12227d0fd
dmitrymedv1-arch <dmitrymedv1@gmail.com>
2026-08-04 07:46:08 +0500

    app.py

--- app.py
+++ app.py
@@ -2425,7 +2425,7 @@ elif st.session_state.app_state.current_step == 2.5:
     with col2:
         st.subheader("Visual Preview")
         
-        if st.session_state.app_state.get('preview_baseline', False):
+        if st.session_state.app_state.preview_baseline:
             x_range = st.session_state.app_state.raw_x
             y_range = st.session_state.app_state.raw_y
             
